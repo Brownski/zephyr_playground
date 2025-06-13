447437cdbf7cc33a99b971ddbf31872f77f7301d
Brownski <browny_297@hotmail.co.uk>
2025-06-13 16:13:52 +0100

    comment

--- src/ble/ble_transport.c
+++ src/ble/ble_transport.c
@@ -3,3 +3,6 @@
 #include <zephyr/bluetooth/conn.h>
 #include <zephyr/bluetooth/uuid.h>
 #include <zephyr/bluetooth/gatt.h>
+
+/* This file will contain an active object and state machine to receive message fragments and reassemble them
+   The active object will then post the assembled message to any subscribed active object               */
